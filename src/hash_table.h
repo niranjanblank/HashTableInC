c6383cb64e43d25cb273f8a02b3212ef5e23f099
Niranjan Shah <niranjanshah474@gmail.com>
2024-09-30 23:13:08 +1000

    hashtable files initialized with definition for item in hashtable and hash table structure

---
+++ src/hash_table.h
@@ -0,0 +1,16 @@
+// This struct will store items containing key and value
+typedef struct {
+  // using pointers for key and value allows dynamic memory allocation
+    char *key;
+    char *value;
+} ht_item;
+
+// this struct will define a hashtable of having total size of "size" and
+// current number of items in hash table, which helps in resizing
+typdef struct {
+  int size;
+  int count;
+  // pointer to an array of pointers to "ht_item"
+  // this allows dynamic memory allocation
+  ht_item **items;
+} ht_hash_table;
